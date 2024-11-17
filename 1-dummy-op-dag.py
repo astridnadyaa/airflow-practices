b2d6f75a1591c949f088cc3ea74a7b6b5917da37
astridnadyaa <73452972+astridnadyaa@users.noreply.github.com>
2024-11-17 14:34:41 +0700

    update comments and add new task (start, end)

--- 1-dummy-op-dag.py
+++ 1-dummy-op-dag.py
@@ -1,28 +1,48 @@
 ### --- Import Libraries ---
 from airflow import DAG
+from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from datetime import datetime
 from airflow.decorators import dag
 
-### --- Default Arguments ---
-default_args = {'owner': 'kribogoreng',
-                'start_date': datetime(2024, 11, 11),
-                'retries': 1}
 
-### --- Define the DAG ---
+### --- Default Arguments ---
+default_args = {
+    'owner': 'kribogoreng',
+    'start_date': datetime(2024, 11, 11),
+    'retries': 1
+}
+
+
+### -- Define the DAG ---
 @dag(
     default_args=default_args,
-    description='Creating DAG using Dummy Operator',
-    schedule_interval='@daily',
+    description='Download File using Bash Operator',
+    schedule_interval='@weekly',
     catchup=False
 )
-def simple_dag():
-    ### --- Define tasks ---
-    start = DummyOperator(task_id='start')
-    end = DummyOperator(task_id='end')
+def download_dag():
+    ### --- Define Tasks ---
+    start = DummyOperator(
+        task_id='start'
+    )
 
-    ### --- Define task dependencies ---
-    start >> end
-  
+    download_file = BashOperator(
+        task_id = 'download_file',
+        bash_command=(
+            'mkdir -p ${AIRFLOW_HOME}/downloads && '
+            'wget -O ${AIRFLOW_HOME}/downloads/yelp.csv ' 
+            '"https://www.dropbox.com/scl/fi/2k8im8ftu9yk8mnqhops9/yelp.csv'
+            '?rlkey=52dzmxgys0su77wb6o75vb5ab&st=lmz21rpk&dl=0'
+        )
+    )
+
+    end = DummyOperator(
+        task_id = 'end'
+    )
+
+    ### --- Define dependencies ---
+    start >> download_file >> end
+
 ### --- Instantiate the DAG ---
-dag = simple_dag()
+dag = download_dag()
